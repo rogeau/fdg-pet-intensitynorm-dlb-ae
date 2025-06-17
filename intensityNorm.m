8f2746ec3cc195f78b4241656e4d0486ccd7cd76
Antoine ROGEAU <antoinerogeau@Antoines-MacBook-Pro.local>
2025-06-17 12:37:44 +0200

    Added QC

--- intensityNorm.m
+++ intensityNorm.m
@@ -1,15 +1,19 @@
 function intensityNorm(input_dir)
     nii_files = dir(fullfile(input_dir, '**', 'w_realigned.nii'));
 
-    % Load wfu mask (pons = 7)
+    % Load ref regions
     wfu_path = 'masks/wfu.nii';
     wfu_header = spm_vol(wfu_path);
     wfu = spm_read_vols(wfu_header);
 
-    % Load GM mask (GM = 1)
     gm_path = 'masks/GM.nii';
     gm_header = spm_vol(gm_path);
     gm = spm_read_vols(gm_header);
+
+    output_pdf = fullfile(input_dir, 'intensity_QC.pdf');
+    if exist(output_pdf, 'file')
+        delete(output_pdf);
+    end
 
     for i = 1:length(nii_files)
         file_path = fullfile(nii_files(i).folder, nii_files(i).name);
@@ -49,6 +53,53 @@ function intensityNorm(input_dir)
             gm_hdr.fname = fullfile(nii_files(i).folder, ['gm_' name ext]);
             spm_write_vol(gm_hdr, norm_gm);
         end
+
+        % Save images for QC
+        mid_x = round(size(pet_vol, 1) / 2);  % Sagittal (X)
+        mid_z = round(size(pet_vol, 3) / 2);  % Axial (Z)
+
+	orig_sag = flipud(squeeze(pet_vol(mid_x, :, :))');
+	pons_sag = flipud(squeeze(pons_mask(mid_x, :, :))');
+        intersect_sag = orig_sag .* double(pons_sag);
+
+	orig_ax = flipud(squeeze(pet_vol(:, :, mid_z))');
+	gm_ax = flipud(squeeze(gm_mask(:, :, mid_z))');
+        intersect_ax = orig_ax .* double(gm_ax);
+
+        fig = figure('Name', 'Sagittal & Axial Visualization', 'NumberTitle', 'off');
+        colormap gray;
+
+        subplot(2, 3, 1);
+        imagesc(orig_sag); axis image off;
+        title('Original');
+        subplot(2, 3, 2);
+        imagesc(pons_sag); axis image off;
+        title('WFU Pons');
+        subplot(2, 3, 3);
+        imagesc(intersect_sag); axis image off;
+        title('Intersection');
+
+        % Bottom row: axial
+        subplot(2, 3, 4);
+        imagesc(orig_ax); axis image off;
+        title('Original');
+
+        subplot(2, 3, 5);
+        imagesc(gm_ax); axis image off;
+        title('GM');
+
+        subplot(2, 3, 6);
+        imagesc(intersect_ax); axis image off;
+        title('Intersection');
+
+	[filepath_parent, file_name, ext] = fileparts(file_path);
+	[filepath_gdparent, parent_folder] = fileparts(filepath_parent);
+	[~, gdparent_folder] = fileparts(filepath_gdparent);
+
+	full_title = sprintf('Spatial Norm QC for: %s/%s/%s%s', gdparent_folder, parent_folder, nii_files(i).name);
+	sgtitle(full_title, 'Interpreter', 'none', 'FontWeight', 'bold', 'FontSize', 10);
+
+        exportgraphics(fig, output_pdf, 'Append', true, 'ContentType', 'image');
+        close(fig);
     end
 end
-
--- spatialNorm.m
+++ spatialNorm.m
@@ -1,21 +1,28 @@
 function spatialNorm(input_dir)
     nii_files = dir(fullfile(input_dir, '**', '*.nii'));
 
+    template = fullfile(pwd, 'template', 'PET.nii');
+
+    output_pdf = fullfile(input_dir, 'norm_QC.pdf');
+    if exist(output_pdf, 'file')
+        delete(output_pdf);
+    end
+
     for i = 1:length(nii_files)
         file_path = fullfile(nii_files(i).folder, nii_files(i).name);
-        
+
         if ~exist(file_path, 'file')
             warning('File not found: %s. Skipping.', file_path);
             continue;
         end
-        
+
         fprintf('🧭 Spatial Normalization... %s\n', file_path);
 
         clear matlabbatch
         matlabbatch{1}.spm.tools.oldnorm.estwrite.subj.source = {file_path};
         matlabbatch{1}.spm.tools.oldnorm.estwrite.subj.wtsrc = '';
         matlabbatch{1}.spm.tools.oldnorm.estwrite.subj.resample = {file_path};
-        matlabbatch{1}.spm.tools.oldnorm.estwrite.eoptions.template = {'/home/arogeau/Desktop/spm12/toolbox/OldNorm/PET.nii,1'};
+        matlabbatch{1}.spm.tools.oldnorm.estwrite.eoptions.template = {template};
         matlabbatch{1}.spm.tools.oldnorm.estwrite.eoptions.weight = '';
         matlabbatch{1}.spm.tools.oldnorm.estwrite.eoptions.smosrc = 8;
         matlabbatch{1}.spm.tools.oldnorm.estwrite.eoptions.smoref = 0;
@@ -29,8 +36,24 @@ function spatialNorm(input_dir)
         matlabbatch{1}.spm.tools.oldnorm.estwrite.roptions.interp = 1;
         matlabbatch{1}.spm.tools.oldnorm.estwrite.roptions.wrap = [0 0 0];
         matlabbatch{1}.spm.tools.oldnorm.estwrite.roptions.prefix = 'w_';
-
         spm_jobman('run', matlabbatch);
+
+        norm_image = fullfile(nii_files(i).folder, ['w_' nii_files(i).name]);
+	
+	spm_check_registration(template, norm_image);
+        fig = spm_figure('FindWin', 'Graphics');
+	
+	[filepath_parent, file_name, ext] = fileparts(file_path);
+	[filepath_gdparent, parent_folder] = fileparts(filepath_parent);
+	[~, gdparent_folder] = fileparts(filepath_gdparent);
+
+	full_title = sprintf('Spatial Norm QC for: %s/%s/%s%s', gdparent_folder, parent_folder, ['w_' nii_files(i).name]);
+	sgtitle(full_title, 'Interpreter', 'none', 'FontWeight', 'bold', 'FontSize', 10);
+        exportgraphics(fig, output_pdf, 'Append', true, 'ContentType', 'image');
     end
+    close all;
+
+    % spm check reg saves a .ps file automatically, this is to remove it
+    delete(fullfile(pwd, 'spm*.ps'));
 end
 
